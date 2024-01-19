cfa990b7ac615958b6ffcd8636bddedf6c473832
WilliamsNathan <149111015+WilliamsNathan@users.noreply.github.com>
2024-01-20 01:34:59 +0200

    no info

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,13 +1,11 @@
 'use client'
 
 import {NextUIProvider} from '@nextui-org/react'
-import { useRouter } from 'next/navigation';
 
 export function Providers({children}: { children: React.ReactNode }) {
-  const router = useRouter();
 
   return (
-    <NextUIProvider navigate={router.push}>
+    <NextUIProvider>
       {children}
     </NextUIProvider>
   )}
